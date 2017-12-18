8009683a091bb15b0945ed0b3779a01145c18de7
zi-kang <hzkdeyx@126.com>
2017-12-18 23:51:15 +0800

    引入koa-bodyparser

--- koa2-tutorial/app.js
+++ koa2-tutorial/app.js
@@ -1,39 +1,54 @@
-/**
- * Created by huzikang on 17/9/30.
- */
-console.log( 'hello world' );
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
 
-app.use(async (ctx, next)=>{
-   if( ctx.request.path === '/' ) {
-       ctx.response.type = 'text/html';
-       ctx.response.body = '<a href="/">index page</a> ' + '<a href="/home">Home page</a> ' + '<a href="/404">404 Not Found</a> ';
-   }else{
-       await next();
-   }
-});
+app.use(bodyParser())
 
+router.get('/', async(ctx, next) => {
+    ctx.response.body = `<h1>index page</h1>`
+})
 
-app.use(async (ctx,next)=>{
-    if( ctx.request.path === '/home' ) {
-        ctx.response.type = 'text/html';
-        ctx.response.body = '<a href="/">index page</a> ' + '<a href="/home">Home page</a> ' + '<a href="/404">404 Not Found</a> '
-    }else{
-        await next();
-    }
-});
-app.use(async (ctx,next)=>{
-    if( ctx.request.path === '/404' ) {
-        ctx.response.type = 'text/html';
-        ctx.response.body = '<a href="/">index page</a> ' + '<a href="/home">Home page</a> ' + '<a href="/404">404 Not Found</a> '
-    }else{
-        await next();
-    }
-});
+router.get('/home', async(ctx, next) => {
+    console.log(ctx.request.query)
+console.log(ctx.request.querystring)
+ctx.response.body = '<h1>HOME page</h1>'
+})
 
+router.get('/home/:id/:name', async(ctx, next)=>{
+    console.log(ctx.params)
+ctx.response.body = '<h1>HOME page /:id/:name</h1>'
+})
 
-app.listen(3000,()=>{
+router.get('/404', async(ctx, next) => {
+    ctx.response.body = '<h1>404 Not Found</h1>'
+})
 
-    console.log( 'server is running at http://localhost:3000' );
-});
+router.get('/user', async(ctx, next)=>{
+    ctx.response.body =
+    `
+    <form action="/user/register" method="post">
+      <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
+      <br/>
+      <input name="password" type="text" placeholder="请输入密码：123456"/>
+      <br/> 
+      <button>GoGoGo</button>
+    </form>
+  `
+})
+
+// 增加响应表单请求的路由
+router.post('/user/register',async(ctx, next)=>{
+    let {name, password} = ctx.request.body
+    if( name == 'ikcamp' && password == '123456' ){
+    ctx.response.body = `Hello， ${name}！`
+}else{
+    ctx.response.body = '账号信息错误'
+}
+})
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000')
+})
