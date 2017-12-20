2ebe90b8ba5b818d59468707792b86c694832aac
huzikang <huzikang@sightp.com>
2017-12-20 13:49:00 +0800

    静态文件

--- controller/home.js
+++ controller/home.js
@@ -2,7 +2,7 @@ const HomeService = require('../service/home')
 
 module.exports = {
     index: async(ctx, next) => {
-        ctx.response.body = `<h1>index page</h1>`
+        await ctx.render("home/index", {title: "iKcamp欢迎您"})
     },
     home: async(ctx, next) => {
         console.log(ctx.request.query)
@@ -14,17 +14,20 @@ module.exports = {
         ctx.response.body = '<h1>HOME page /:id/:name</h1>'
     },
     login: async(ctx, next) => {
-        await ctx.render('home/login',{
-            btnName: 'GoGoGo',
-            placeholders: ['请输入用户名：kunt', '请输入密码：123456']
+        await ctx.render('home/login', {
+            btnName: 'GoGoGo'
         })
     },
     register: async(ctx, next) => {
-        let {
-            name,
-            password
-        } = ctx.request.body
-        let data = await HomeService.register(name, password)
-        ctx.response.body = data
+        let params = ctx.request.body
+        let name = params.name
+        let password = params.password
+        let res = await HomeService.register(name,password)
+        if(res.status == "-1"){
+            await ctx.render("home/login", res.data)
+        }else{
+            ctx.state.title = "个人中心"
+            await ctx.render("home/success", res.data)
+        }
     }
 }
