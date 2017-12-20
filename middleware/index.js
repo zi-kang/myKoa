8f6b6c898fe5a919209d93f6c3a43a89d213509c
huzikang <huzikang@sightp.com>
2017-12-20 14:05:50 +0800

    接入json数据

--- middleware/index.js
+++ middleware/index.js
@@ -1,32 +1,20 @@
-const path = require("path")
-const ip = require("ip")
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+const staticFiles = require('koa-static')
 
-// const miLog = require('./mi-log')
-
+const miSend = require('./mi-send')
 module.exports = (app) => {
+    app.use(staticFiles(path.resolve(__dirname, "../public")))
 
+    app.use(nunjucks({
+        ext: 'html',
+        path: path.join(__dirname, '../views'),
+        nunjucksConfig: {
+            trimBlocks: true
+        }
+    }));
 
-    /**
-     * 记录URL以及页面执行时间
-     */
-    app.use(async (ctx, next) => {
-        let start = Date.now()
-        await next()
-        let delta = Date.now() - start
-        ctx.log && ctx.log.info({
-            responseTime: delta
-        })
-    })
-
-    /**
-     * 初始化log
-     */
-    // app.use(miLog(app.env, {
-    //   env: app.env,
-    //   projectName: 'node-tutorial',
-    //   appLogLevel: 'debug',
-    //   dir: 'logs',
-    //   serverIp: ip.address()
-    // }));
-
+    app.use(bodyParser())
+    app.use(miSend())
 }
